c060cf427bd4ed0111e5a6be8dca95a3ebf93c49
Gabin-ishimwe <g.ishimwe@alustudent.com>
2022-05-26 15:20:25 +0200

    setup material ui

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../view/home';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../theme/theme';
 
 const AllRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
